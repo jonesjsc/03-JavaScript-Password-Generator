a37b3286e1ff6d0a78ae8a2ddfc5385f819521ef
Jason Jones <jason.e.jones@gmail.com>
2021-02-07 16:00:33 -0500

    Working homework assignment

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// to build your string with the passwored just use += 
+const passwordMinLength = 8;    // minimum length of generated password
+const passwordMaxLength = 128;  // maximum length of generated password
 
-var i, j;
+// var i, j;
 
-var upperCaseArray  = makeCharArray(65,90);  // upper case letters are ascii 65 - 90
-var lowerCaseArray  = makeCharArray(97,122); // lower case letters are ascii 97 - 122
-var numbersArray    = makeCharArray(48,57);    // numbers are ascii 48 - 57
-var specialsArray   = makeCharArray(32,47) +  // owasp specials block 1 - ASCII 32 thru 47
-                      makeCharArray(58,64) +  // owasp specials block 2 - ASCII 58 thru 64 
-                      makeCharArray(91,96) +  // owasp specials block 3 - ASCII 91 thru 96
-                      makeCharArray(123,126); // owasp specials block 4 - ASCII 123 thru 126 
-
-action=window.confirm("did you drink ACV today?");
-
-if(action) {
-  window.alert("you hit ok");
-}
-  else {
-window.alert("you hit cancel");
-}                  
+var upperCaseArray  = makeCharArray(65,90);                         // upper case letters are ascii 65 - 90
+var lowerCaseArray  = makeCharArray(97,122);                        // lower case letters are ascii 97 - 122
+var numbersArray    = makeCharArray(48,57);                         // numbers are ascii 48 - 57
+var specialsArray   = makeCharArray(32,47);                         // owasp specials block 1 - ASCII 32 thru 47
+var specialsArray   = specialsArray.concat(makeCharArray(58,64));   // owasp specials block 2 - ASCII 58 thru 64 
+var specialsArray   = specialsArray.concat(makeCharArray(91,96));   // owasp specials block 3 - ASCII 91 thru 96
+var specialsArray   = specialsArray.concat(makeCharArray(123,126)); // owasp specials block 4 - ASCII 123 thru 126 
 
 // ------------------------------------------------------------------------------
 
@@ -42,8 +34,69 @@ function makeCharArray(lowerRange,upperRange) {
   return (returnArray);
 }
 
+
+/**
+ * Return a random integer between 0 and max
+ * @param  {Number} max           lower range of the ASCII table
+ * @return {Number} randomNumber  a Random Number
+ */
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+/**
+ * Return a random integer between 0 and max
+ * @param  {} NULL                       no inputs expected
+ * @return {String} passwordPlaceholder  a String composed of randomly determined array elements based on uiser input
+ */
+
 function generatePassword () {
-  return ("wassup dawg");
+  keepAskin = true;
+  while (keepAskin) { 
+    passwordLength = window.prompt("How many characters in your password? (8-128)",8);
+    if (passwordLength >= passwordMinLength && passwordLength <= passwordMaxLength) {
+      keepAskin = false;
+    } else {
+      window.alert("password length must be number between 8 and 128");
+    }
+  }
+
+  bIncludeUpper = window.confirm("Include UPPER CASE letters? [OK] = yes [Cancel] = no");
+  bIncludeNumeral = window.confirm("Include numerials [OK] = yes [Cancel] = no");
+  bIncludeSpecial = window.confirm("Include Special Characters? [OK] = yes [Cancel] = no");
+
+  if (!bIncludeUpper && !bIncludeNumeral && !bIncludeSpecial) {
+    window.alert("imma go ahead an just assume you want only lower case then.");
+    bIncludeLower = true;
+  } else
+
+  bIncludeLower = window.confirm("Include lower case letters? [OK] = yes [Cancel] = no");
+
+  var resplendentArrayOfAllPossibilities = [];
+
+  if (bIncludeUpper) {
+    resplendentArrayOfAllPossibilities = resplendentArrayOfAllPossibilities.concat(upperCaseArray); 
+  }
+
+  if (bIncludeLower) {
+    resplendentArrayOfAllPossibilities = resplendentArrayOfAllPossibilities.concat(lowerCaseArray); 
+  }
+
+  if (bIncludeNumeral) {
+    resplendentArrayOfAllPossibilities = resplendentArrayOfAllPossibilities.concat(numbersArray);
+  }
+
+  if (bIncludeSpecial) {
+    resplendentArrayOfAllPossibilities = resplendentArrayOfAllPossibilities.concat(specialsArray); 
+  }
+
+  var passwordPlaceholder = "";
+  for (i = 0; i < passwordLength; i++) {
+    passwordPlaceholder += resplendentArrayOfAllPossibilities[getRandomInt(resplendentArrayOfAllPossibilities.length)]
+  }
+
+  return (passwordPlaceholder);
 }
 
 // Write password to the #password input
@@ -51,9 +104,7 @@ function writePassword() {
   console.log ("word");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-  // passwordText.value = "testing";
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 03-JavaScript-Password-Generator
 1st Javascript assignment
-links 
-ASCII table https://www.rapidtables.com/code/text/ascii-table.html
-
+# links 
+* https://www.rapidtables.com/code/text/ascii-table.html
 * https://owasp.org/www-community/password-special-characters
 * https://docs.google.com/spreadsheets/d/17oWPJ3x0biuTS77ADcNflrXFdUI-YjPXpGfeJ3DG5m8/edit#gid=0
+* https://www.w3schools.com/jsref/jsref_fromcharcode.asp
